4cc4129af6866e613e908c451e8dd9ef1fcd373a
SEdilson <edilson.silva00@hotmail.com>
2019-11-13 09:22:47 -0300

    Fixing pagination

--- company_service/settings/base.py
+++ company_service/settings/base.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
 }
 
 SIMPLE_JWT = {

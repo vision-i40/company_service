1feaa1e6802025361a88308e5e7fc2d87b92c3c7
SEdilson <edilson.silva00@hotmail.com>
2019-11-26 12:21:55 -0300

    Refactoring signals

--- company_service/signals.py
+++ company_service/signals.py
@@ -6,31 +6,30 @@ from django.db.models import Q, Min, Max
 @receiver(post_save, sender=ManualStop)
 def create_state_events(sender, **kwargs):
     manual_stop = ManualStop.objects
+    start_datetime = manual_stop.values('start_datetime').last()['start_datetime']
+    end_datetime = manual_stop.values('end_datetime').last()['end_datetime']
+    def set_state_events(datetime):
+        StateEvent.objects.create(production_line_id=manual_stop.values('production_line_id').last()['production_line_id'], 
+                            stop_code_id=manual_stop.values('stop_code_id').last()['stop_code_id'], 
+                            event_datetime=datetime, 
+                            state=StateEvent.OFF)        
     if kwargs.get('created', True):
         if manual_stop.values('start_datetime'):
-            StateEvent.objects.create(production_line_id=manual_stop.values('production_line_id').last()['production_line_id'], 
-                                        stop_code_id=manual_stop.values('stop_code_id').last()['stop_code_id'], 
-                                        event_datetime=manual_stop.values('start_datetime').last()['start_datetime'], 
-                                        state=StateEvent.OFF)
+            set_state_events(start_datetime)
         if manual_stop.values('end_datetime'):
-            StateEvent.objects.create(production_line_id=manual_stop.values('production_line_id').last()['production_line_id'], 
-                                        stop_code_id=manual_stop.values('stop_code_id').last()['stop_code_id'], 
-                                        event_datetime=manual_stop.values('end_datetime').last()['end_datetime'], 
-                                        state=StateEvent.OFF)
+            set_state_events(end_datetime)
 
 @receiver(post_save, sender=StateEvent)
 def create_availability_instance(sender, **kwargs):
     state_event = StateEvent.objects
+    def set_availability_instance(state):
+        Availability.objects.create(production_line_id=state_event.values('production_line_id').last()['production_line_id'], 
+                                start_time=state_event.filter(state=StateEvent.ON).aggregate(Min('event_datetime'))['event_datetime__min'],
+                                end_time=state_event.filter(state=StateEvent.ON).aggregate(Max('event_datetime'))['event_datetime__max'], 
+                                stop_code_id=state_event.values('stop_code_id').last()['stop_code_id'],
+                                state=state)
     if kwargs.get('created', True):
         if StateEvent.objects.values('state').last()['state'] == StateEvent.ON:
-            Availability.objects.create(production_line_id=state_event.values('production_line_id').last()['production_line_id'], 
-                                            start_time=state_event.filter(state=StateEvent.ON).aggregate(Min('event_datetime'))['event_datetime__min'],
-                                            end_time=state_event.filter(state=StateEvent.ON).aggregate(Max('event_datetime'))['event_datetime__max'], 
-                                            stop_code_id=state_event.values('stop_code_id').last()['stop_code_id'],
-                                            state=StateEvent.ON)
+            set_availability_instance(StateEvent.ON)
         elif StateEvent.objects.values('state').last()['state'] == StateEvent.OFF:
-            Availability.objects.create(production_line_id=state_event.values('production_line_id').last()['production_line_id'], 
-                                start_time=state_event.filter(state=StateEvent.OFF).aggregate(Min('event_datetime'))['event_datetime__min'], 
-                                end_time=state_event.filter(state=StateEvent.OFF).aggregate(Max('event_datetime'))['event_datetime__max'], 
-                                stop_code_id=state_event.values('stop_code_id').last()['stop_code_id'],
-                                state=StateEvent.OFF)
+            set_availability_instance(StateEvent.OFF)
